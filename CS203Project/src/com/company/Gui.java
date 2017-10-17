b7a3ec09b81eb0e54bc336c26348d9c703bbcd99
lopezl <lopezl@lafayette.edu>
2017-10-17 14:26:32 -0400

    Added flags and registers to GUI.

--- CS203Project/src/com/company/Gui.java
+++ CS203Project/src/com/company/Gui.java
@@ -8,28 +8,42 @@ public class Gui extends Frame implements ActionListener{
     // instance variables - replace the example below with your own
     private Label     register_label;  // Declare a Label component
     private TextField register_value;  // Declare a TextField component
+    private Label     z_label;  // Declare a Label component
+    private Label     n_label;  // Declare a Label component
+    private Label     c_label;  // Declare a Label component
+    private Label     v_label;  // Declare a Label component
+    private Label     sp_label;  // Declare a Label component
+
+
+
+
     private Button    time_step;       // Declare a Button component
     private Button    exit_button;     // Declare a Button component
-    private JList mem = new JList();
+   // private JList registers;
     private JScrollPane myScrollPane = new JScrollPane();
+    private JScrollPane myScrollPaneReg = new JScrollPane();
+    private JScrollPane myScrollPaneFlag = new JScrollPane();
+
     private JTextArea memMap;
+    private JTextArea registers;
 
 
     private int time = 0;              // Counter's value
     private  Translator trans;
     private  Cpu cpu;
     private TextField pc;
+    private TextField sp;
+
     private TextField ir;
-    private TextField ma;
-    private TextField md;
-    private TextField c;
-    private TextField a;
-    private TextField instruction;
-    private ArrayList<TextField> registers;
-    private TextField o;
-    private TextField s;
     private TextField z;
-    private TextField q;
+    private TextField n;
+    private TextField c;
+    private TextField v;
+
+
+
+    private TextField instruction;
+
 
 
     public Gui(Translator aTrans )
@@ -74,37 +88,100 @@ public class Gui extends Frame implements ActionListener{
 
 
 
-        Panel p8 = new Panel();
-        p8.setLayout(new FlowLayout());
+        Panel p4 = new Panel();
+        p4.setLayout(new FlowLayout());
 
         register_label = new Label("Instruction: ");     // construct the Label component
-        p8.add(register_label);                 // "super" Frame adds Label
+        p4.add(register_label);                 // "super" Frame adds Label
 
         instruction = new TextField(" ", 20); // construct the TextField component
         instruction.setEditable(false);       // set to read-only
-        p8.add(instruction);                  // "super" Frame adds TextField
+        p4.add(instruction);                  // "super" Frame adds TextField
         instruction.setText(cpu.getCurrentInstruction());
         ir.setText(cpu.getCurrentBinaryInstruction());
 
 
 
+        Panel p5 = new Panel();
+        p5.setLayout(new FlowLayout());
+        z_label = new Label("Z: ");     // construct the Label component
+        p5.add(z_label);                 // "super" Frame adds Label
+
+        z = new TextField("0", 1); // construct the TextField component
+        z.setEditable(false);       // set to read-only
+
+        p5.add(z);
+
+        Panel p6 = new Panel();
+        p6.setLayout(new FlowLayout());
+        n_label = new Label("N: ");     // construct the Label component
+        p6.add(n_label);                 // "super" Frame adds Label
+
+        n = new TextField("0", 1); // construct the TextField component
+        n.setEditable(false);       // set to read-only
+
+        p6.add(n);
+
+
+        Panel p7 = new Panel();
+        p7.setLayout(new FlowLayout());
+        c_label = new Label("C: ");     // construct the Label component
+        p7.add(c_label);                 // "super" Frame adds Label
+
+        c = new TextField("0", 1); // construct the TextField component
+        c.setEditable(false);       // set to read-only
+
+        p7.add(c);
+
+        Panel p8 = new Panel();
+        p8.setLayout(new FlowLayout());
+        v_label = new Label("V: ");     // construct the Label component
+        p8.add(v_label);                 // "super" Frame adds Label
+
+        v = new TextField("0", 1); // construct the TextField component
+        v.setEditable(false);       // set to read-only
+
+        p8.add(v);
+
+        Panel p9 = new Panel();
+        p9.setLayout(new FlowLayout());
+
+        sp_label = new Label("SP: ");     // construct the Label component
+        p9.add(sp_label);                 // "super" Frame adds Label
+
+        sp= new TextField("0", 10); // construct the TextField component
+        sp.setEditable(false);       // set to read-only
+        sp.setText("0x0");
+        p9.add(sp);
+
         memMap = new JTextArea(trans.retreiveMemImage());
-        memMap.setColumns(50);
-        memMap.setRows(30);
+        registers = new JTextArea(cpu.getRegisters());
+       // registers = new JList(cpu.getRegisters());
+        memMap.setColumns(30);
+        memMap.setRows(20);
         myScrollPane.setViewportView(memMap);
+        myScrollPaneReg.setViewportView(registers);
         memMap.setEditable(false);
+        registers.setEditable(false);
 
         setTitle("Simulator GUI");  // "super" Frame sets its title
         setSize(550, 500);             // "super" Frame sets its initial window size
 
         // add subpanels to the primary frame
-        add(p8);
         add(p1);
         add(p2);
         add(p3);
+        add(p4);
+        add(p5);
+        add(p6);
+        add(p7);
+        add(p8);
+        add(p9);
+
 
 
         add(myScrollPane);
+        add(myScrollPaneReg);
 
 
         setVisible(true);
@@ -112,11 +189,21 @@ public class Gui extends Frame implements ActionListener{
 
     @Override
     public void actionPerformed(ActionEvent evt) {
-        if (evt.getActionCommand().equals("Step") ) {
+        if (evt.getActionCommand().equals("Step") && !cpu.getCurrentInstruction().contains("HALT") ) {
             cpu.getNextInstruction();
             instruction.setText(cpu.getCurrentInstruction());
             ir.setText(cpu.getCurrentBinaryInstruction());
             pc.setText(cpu.getCurrentLocation());
+        }
+
+        else if (evt.getActionCommand().equals("Exit")) {
+
+            System.exit(0);  // Terminate the program
+
+        }
+        else if(cpu.getCurrentInstruction().contains("HALT"))
+        {
+            System.out.println();
         }
 
         }
