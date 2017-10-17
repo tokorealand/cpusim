11666f8f8009f3a832aacac63003705577d1a3b3
lopezl <lopezl@lafayette.edu>
2017-10-17 14:25:59 -0400

    Added registers to project simulation.

--- CS203Project/src/com/company/Register.java
+++ CS203Project/src/com/company/Register.java
@@ -1,4 +1,31 @@
 package com.company;
 
 public class Register {
+    int[] registers;
+
+
+    public Register(int numberRegister)
+    {
+        registers=new int[numberRegister];
+        for (int reg:registers) {
+            reg=0;
+
+        }
+    }
+
+    public int getRegister(int registerNumber) {
+
+
+        return registers[registerNumber];
+
+    }
+
+    public void setRegisters(int registerNumber, int value)
+    {
+        registers[registerNumber] = value;
+    }
+
+    public int[] getRegisters() {
+        return registers;
+    }
 }
