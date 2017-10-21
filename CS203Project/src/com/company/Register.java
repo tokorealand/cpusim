b925d5c2a77d375e96346993461a680398e6f3b5
lopezl <lopezl@lafayette.edu>
2017-10-20 22:47:22 -0400

    Changes registers to contain strings instead of ints.

--- CS203Project/src/com/company/Register.java
+++ CS203Project/src/com/company/Register.java
@@ -1,31 +1,39 @@
 package com.company;
 
 public class Register {
-    int[] registers;
+    String[] registers;
 
 
     public Register(int numberRegister)
     {
-        registers=new int[numberRegister];
-        for (int reg:registers) {
-            reg=0;
+        registers=new String[numberRegister];
+        for(int i=0; i<registers.length; i++)
+        {
+            registers[i]="00000000000000000000000000000000";
+        }
 
+    }
+
+    public  void print()
+    {
+        for (String reg:registers) {
+        System.out.println(reg);
         }
     }
 
-    public int getRegister(int registerNumber) {
+    public String getRegister(int registerNumber) {
 
 
         return registers[registerNumber];
 
     }
 
-    public void setRegisters(int registerNumber, int value)
+    public void setRegisters(int registerNumber, String value)
     {
         registers[registerNumber] = value;
     }
 
-    public int[] getRegisters() {
+    public String[] getRegisters() {
         return registers;
     }
 }
