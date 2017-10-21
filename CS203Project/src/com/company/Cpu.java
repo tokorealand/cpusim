a68ffa832d70b4cdc2fdf82d9e02a17c75e08107
lopezl <lopezl@lafayette.edu>
2017-10-20 22:43:57 -0400

    Added stack parameters to cpu.

--- CS203Project/src/com/company/Cpu.java
+++ CS203Project/src/com/company/Cpu.java
@@ -1,34 +1,40 @@
 package com.company;
+import java.math.BigInteger;
+
 
 public class Cpu {
   private   InstructionSet instructionSet;
   private   Memory memory;
   private   Register reg;
+  private   String stack="0x0";
   private   int currentByte=0;
   private   String currentInstruction="";
   private   String currentBinaryInstruction="";
-  private   boolean[] flags = new boolean[4];
+  private   int[] flags = {0,0,0,0}; //pos 0-zero 1-negative 2-carry 3-overflow
 
 
     public Cpu(InstructionSet iSA, Memory mem, int registerCount)
     {
         instructionSet=iSA;
         memory=mem;
+        stack=mem.getStackPointer();
         reg=new Register(registerCount);
-        getNextInstruction();
-        initiateFlags();
+        instructionSet.establishOperator(reg,flags,memory);
+        reg.print();
+        System.out.println("SSSSSSSSss");
+        memory.printMap();
 
 
 
     }
 
-    void initiateFlags()
+    public String getStack()
     {
-        for(boolean flag : flags)
-        {
-            flag=false;
-        }
+        return stack;
     }
+
+
+
 
 
 
@@ -52,18 +58,39 @@ public class Cpu {
 
     String getRegisters()
     {
-        int[] regC=reg.getRegisters();
+        String[] regC=reg.getRegisters();
         String stringRegs="";
        for(int i=0; i<regC.length; i++)
        {
-           stringRegs+="X"+String.valueOf(i)+" = "+String.valueOf(regC[i]) +"\n";
+           System.out.println(regC[i]);
+           System.out.println(new BigInteger(regC[i],2).intValue());
+           System.out.println("SSSSSSSSSSSSSSSSSSSSSSSSSS");
+           stringRegs+="X"+String.valueOf(i)+" = "+String.valueOf(new BigInteger(regC[i],2).intValue()) +"\n";
        }
+       System.out.println(stringRegs);
        return stringRegs;
     }
-    void getNextInstruction()
+
+
+
+    public String getRequestedFlag(int i)
     {
+        return String.valueOf(flags[i]);
+    }
+
+    public void clearFlags()
+    {
+        flags[0]=0;
+        flags[1]=0;
+        flags[2]=0;
+        flags[3]=0;
+    }
+
+    void fetchAndExecute()
+    {
+        clearFlags();
         if(currentByte+4<memory.getMaxSize()) {
-             currentBinaryInstruction = memory.retriveInstruction(currentByte);
+             currentBinaryInstruction = memory.retriveWord(currentByte);
             currentInstruction =instructionSet.parseMemoryForInstruction(currentBinaryInstruction);
             currentByte+=4;
         }
