d7e9222a5e1634615d8724f1fc9ba6d8e8506460
lopezl <lopezl@lafayette.edu>
2017-10-22 13:48:11 -0400

    Added javadoc and a tostring method.

--- CS203Project/src/com/company/Register.java
+++ CS203Project/src/com/company/Register.java
@@ -1,9 +1,16 @@
 package com.company;
 
+/**
+ * Registers are the non memory storage medium that the Cpu has available for working.
+ */
 public class Register {
-    String[] registers;
+    String[] registers; //array containing the values of the registers pos 0 = register X0...
 
 
+    /**
+     * Initializes all registers to full 32 bit binary zero.
+     * @param numberRegister how many registers are being created
+     */
     public Register(int numberRegister)
     {
         registers=new String[numberRegister];
@@ -21,6 +28,11 @@ public class Register {
         }
     }
 
+    /**
+     *
+     * @param registerNumber  the index of register
+     * @return requested register
+     */
     public String getRegister(int registerNumber) {
 
 
@@ -28,11 +40,24 @@ public class Register {
 
     }
 
+    /**
+     *
+     * @param registerNumber requested register to be changed
+     * @param value to be inserted into register
+     */
     public void setRegisters(int registerNumber, String value)
     {
+        if(Translator.noisy.equals("true"))
+        {
+            System.out.println("X"+registers[registerNumber]+" Being changed to " + value );
+        }
         registers[registerNumber] = value;
     }
 
+    /**
+     *
+     * @return all the registers.
+     */
     public String[] getRegisters() {
         return registers;
     }
