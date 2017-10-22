f03d92b9598081281a03333d7efea2cf9edb244a
lopezl <lopezl@lafayette.edu>
2017-10-22 13:44:41 -0400

    Added args[] and trycatch to Main.

--- CS203Project/src/com/company/Main.java
+++ CS203Project/src/com/company/Main.java
@@ -5,13 +5,20 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
+try {
+    Translator t = new Translator(args[0],args[1],args[2],args[3]);
 
-        Translator t = new Translator();
-        System.out.println("DDDDD");
+    t.readMemFile("D");
 
-        t.readAssemblyFile("D");
+    Gui gu = new Gui(t);
 
-        Gui gu = new Gui(t);
+}
+catch (ArrayIndexOutOfBoundsException e)
+{
+    System.out.println("Wrong number of arguments");
+}
+
+
 
     }
 }
