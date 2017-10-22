432e80af9fd1c1441cc78e4ce46939473ca39326
lopezl <lopezl@lafayette.edu>
2017-10-22 13:46:37 -0400

    Added java doc and a reset and backup method.

--- CS203Project/src/com/company/Memory.java
+++ CS203Project/src/com/company/Memory.java
@@ -2,20 +2,23 @@ package com.company;
 
 import java.util.ArrayList;
 
-class Memory {
+public class Memory {
 
     private ArrayList<Byte> memory = new ArrayList<Byte>();
+    private ArrayList<Byte> memoryBackUp = new ArrayList<Byte>();
 
     private int currentIndex=0;
     private int wordSize;
     private int maxSize;
-    private String stackPointer;
+    private String stackPointer="0x0";
+    private String linkPointer;
 
 
     public static void main(String args[])
     {
 
     }
+
 
         Memory(int maxSize, int wordSize, String stack)
         {
@@ -25,6 +28,27 @@ class Memory {
 
 
         }
+
+        public void setLinkPointer(String lp) { linkPointer=lp; }
+
+    public void pushStack(String wordPushed)
+    {
+
+        storeWord(Integer.parseInt(stackPointer.substring(2),16),wordPushed);
+        stackPointer="0x"+Integer.toHexString(Integer.parseInt(stackPointer.substring(2),16)+4);
+    }
+
+    public String popStack()
+    {
+       String holder = retriveWord(Integer.parseInt(stackPointer.substring(2),16)-4);
+        stackPointer="0x"+Integer.toHexString(Integer.parseInt(stackPointer.substring(2),16)-4);
+        return holder;
+
+    }
+
+
+
+    public String getLinkPointer(){return linkPointer;}
 
         public void storeWord(int location, String word)
         {
@@ -36,8 +60,7 @@ class Memory {
 
     void setWordInMemory(String command)
     {
-        System.out.println(command);
-        System.out.println("COMMADE");
+
         String parser="";
         char[] com = command.toCharArray();
 
@@ -53,15 +76,26 @@ class Memory {
 
     }
 
+   public void setMemoryBackUp()
+    {
+        for (int i=0; i<memory.size(); i++)
+        {
+            memoryBackUp.add(new Byte(memory.get(i)));
+        }
+
+    }
+
+    int getCurrentIndex(){return currentIndex; }
+
         String retriveWord(int byteNum)
         {
             String bitCommand="";
-            System.out.println("QQQQ");
-            System.out.println(byteNum);
+
                 for (int i = 0; i < 4; i++) {
                     bitCommand += memory.get(byteNum + i).getBinary();
 
             }
+            currentIndex+=4;
             return bitCommand;
         }
 
@@ -89,8 +123,7 @@ class Memory {
 
         void addInstructionToMemory(String command)
         {
-            System.out.println(command);
-            System.out.println("COMMADE");
+
             String parser="";
             char[] com = command.toCharArray();
 
@@ -127,11 +160,40 @@ class Memory {
             }
         }
 
+   public void reset()
+    {
+        memory=memoryBackUp;
+
+    }
 
 
 
 
 
+    public void printHex()
+    {
+        System.out.println("WS-"+Integer.toString(wordSize)+":MM-"+Integer.toString(maxSize) +":SP-"+stackPointer);
+        String border = new String(new char[(wordSize/8)*6]).replace("\0", "-");
+
+        for (int i=0; i<memory.size(); i++)
+        {
+            if(i%(wordSize/8)==0)
+            {
+                System.out.println(" ");
+                System.out.println(border);
+                System.out.print("0x"+Integer.toHexString(i));
+                System.out.print("|");
+            }
+
+            System.out.print(memoryBackUp.get(i).getHex());
+            System.out.print("|");
+
+
+        }
+        System.out.println(" ");
+        System.out.println(border);
+
+    }
 
 
 
@@ -178,6 +240,33 @@ class Memory {
             }
 
             map+=memory.get(i).getBinary();
+            map+="|";
+
+
+        }
+        map+="\n";
+        map+=border+"\n";
+        return  map;
+
+    }
+
+    String stringMapHex()
+    {
+        String map = "WS-"+Integer.toString(wordSize)+":MM-"+Integer.toString(maxSize) +":SP-"+stackPointer;
+        String border = new String(new char[(wordSize/8)*10]).replace("\0", "-");
+
+        for (int i=0; i<memory.size(); i++)
+        {
+            if(i%(wordSize/8)==0)
+            {
+                map+="\n";
+                map+=border+"\n";
+                map+="0x"+Integer.toHexString(i);
+                map+="|";
+
+            }
+
+            map+=memory.get(i).getHex();
             map+="|";
 
 
